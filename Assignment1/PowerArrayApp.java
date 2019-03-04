2aac7ff3e161d1402d68100adba86bb348f32f4b
Nicholas <antnic008@myuct.ac.za>
2019-03-04 19:08:10 +0200

    Completed adding command line arguments for testing

--- Assignment1/PowerArrayApp.java
+++ Assignment1/PowerArrayApp.java
@@ -7,13 +7,31 @@ public class PowerArrayApp {
    public static void main(String[] args) {
 	    opCount count = new opCount(0);
 	    String CSVName = "/home/marshmewllow/Desktop/Engineering/2019/CSC2001F/MyRepo/Assignment1/cleaned_data.csv";
-	    timeStamp[] powerReadings = CSVread(CSVName, count);
+	
+  		int lines = 509;
+  		if(args.length > 0) {
+	  		if("-l" .equals(args[0])) {
+	  			if(args.length > 1) {
+	  				lines = (Integer.valueOf(args[1]));
+	  			}
+	  		}
+  		}
+  		timeStamp[] powerValues = new timeStamp[Integer.valueOf(lines)];
+	    timeStamp[] powerReadings = CSVread(CSVName, count,powerValues, lines);
 	    count.opCount = count.opCount + 1;
 	  	if(args.length > 0) {
 	  		if("-c" .equals(args[0])) {
 	  			printAllDateTimes(powerReadings, count);
 	  			System.out.println("Total operation count = " +count.opCount);	
-	  		}else {
+	  		}else if("-l" .equals(args[0])){
+	  			printAllDateTimes(powerReadings, count);
+	  			if(args.length > 2) {
+		  			if("-c" .equals(args[2])) {
+			  			System.out.println("Total operation count = " +count.opCount);	
+			  		}
+	  			}
+	  		}
+	  		else {
 		  		printDateTime(powerReadings, args[0], count);  		
 		  		for(int i = 0; i < (args.length) ; i++) {
 		  			if("-c" .equals(args[i])) {
@@ -27,7 +45,7 @@ public class PowerArrayApp {
 		  		for(int i = 0; i < (args.length) ; i++) {
 		  			if(args[i] == "-c") {
 		  				System.out.println("Total operation count = " +count.opCount);	
-		  			}
+		  		}
 		  		}
 		}
    }
@@ -73,36 +91,34 @@ public class PowerArrayApp {
 		}
    }
 	
-   public static timeStamp[] CSVread(String FileName, opCount count){
+   public static timeStamp[] CSVread(String FileName, opCount count, timeStamp[] powerValues, int end){
        String line = "";
 	   //Declaring array
-	   timeStamp[] powerValues;
-	   
-	   //Adjust this value to control data set size
-	   powerValues = new timeStamp[510];	
-	
-	   
+       
 	   try (BufferedReader br = new BufferedReader(new FileReader(FileName))) {
 	
 		   int lineNo = 0;
 		   count.opCount = count.opCount + 1;
-	       while ((line = br.readLine()) != null) {
-	   	    count.opCount = count.opCount + 1;
-	           String[] Element = line.split(",");
-	       
-	       int i = 0;
-	       int size = Element.length;
-	       count.opCount = count.opCount + 1;
-	       while(i < size) {
-	   	    	count.opCount = count.opCount + 1;
-	   	    	count.opCount = count.opCount + 1;
-	    	   	if (lineNo > 0) {
-		   	       powerValues[lineNo] = new timeStamp(Element[3],Element[1],Element[0]);
-	    	   	}	   	       
-	    	   	i ++;	
-	       }               
-	       lineNo ++;
-	       }
+		   if (lineNo < (end + 1)) {
+			   while ((line = br.readLine()) != null) {
+		    	   count.opCount = count.opCount + 1;
+		           String[] Element = line.split(",");				       		
+			       int i = 0;
+			       int size = Element.length;
+			       count.opCount = count.opCount + 1;
+			       while(i < size) {
+			   	    	count.opCount = count.opCount + 1;
+			   	    	count.opCount = count.opCount + 1;
+			    	   	if (lineNo <(end) ) {
+				   	       powerValues[lineNo] = new timeStamp(Element[3],Element[1],Element[0]);
+			    	   	}	   	       
+			    	   	i ++;	
+			       }    
+			       
+			      lineNo ++;
+		       }
+			   
+		   }
 	
 	       } 
 	       catch (IOException e) {
